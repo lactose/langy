949a223c60ec0e6acdb55aa8563e0012b997727b
Zachary Owen <dev@scrptr.com>
2012-07-19 21:45:50 -0400

    fixed dev

--- app.js
+++ app.js
@@ -78,14 +78,12 @@ var app = express();
 
 
 app.configure(function(){
-  initRedis();
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.cookieParser());
-  app.use(express.session({ secret: 'dadaism', store: new RedisStore, cookie: { maxAge: 60000*( (60*24) * 30)} })); // 30 days
   app.use(everyauth.middleware());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -94,10 +92,13 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.session({secret: 'dadaism'}));
   app.set('port', 3000);
 });
 
 app.configure('production', function() {
+  initRedis();
+  app.use(express.session({ secret: 'dadaism', store: new RedisStore, cookie: { maxAge: 60000*( (60*24) * 30)} })); // 30 days
   var logFile = fs.createWriteStream(__dirname + '/logs/production.log', {flags: 'a'});
   app.use(express.errorHandler());
   app.use(express.logger({stream: logFile}));
